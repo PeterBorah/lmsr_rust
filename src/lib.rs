cccb4da1587b2ea453d6e6cfae7c8183f8e4ec19
Peter Borah <peterborah@gmail.com>
2018-12-20 18:53:36 -0800

    Switch to using methods

--- src/lib.rs
+++ src/lib.rs
@@ -9,33 +9,31 @@ pub struct Market {
     pub outstanding_shares: Vec<f64>,
 }
 
-pub struct Trade {
-    pub outcome_id: usize,
-    pub shares: f64
-}
+impl Market {
+    pub fn cost_fn(&self) -> f64 {
+        self.b * self.sum_of_exp().ln()
+    }
 
-pub fn cost_fn(market: &Market) -> f64 {
-    market.b * sum_of_exp(market).ln()
-}
+    // Calculates exp(q1/b) for each outcome and sums
+    fn sum_of_exp(&self) -> f64 {
+        self.outstanding_shares.iter().fold(0_f64, |acc, q| acc + E.pow(q / self.b))
+    }
 
-fn sum_of_exp(market: &Market) -> f64 {
-    market.outstanding_shares.iter().fold(0_f64, |acc, q| acc + E.pow(q / market.b))
-}
+    pub fn cost_to_trade(&self, outcome_id: usize, shares: f64) -> f64 {
+        let mut new_market = self.clone();
+        new_market.outstanding_shares[outcome_id] += shares;
 
-pub fn cost_to_trade(market: &Market, trade: &Trade) -> f64 {
-    let mut new_market = market.clone();
-    new_market.outstanding_shares[trade.outcome_id] += trade.shares;
+        new_market.cost_fn() - self.cost_fn()
+    }
 
-    cost_fn(&new_market) - cost_fn(market)
-}
+    pub fn price(&self, idx: usize) -> f64 {
+        E.pow(self.outstanding_shares[idx] / self.b) / self.sum_of_exp()
+    }
 
-pub fn price(market: &Market, idx: usize) -> f64 {
-    E.pow(market.outstanding_shares[idx] / market.b) / sum_of_exp(market)
-}
-
-pub fn shares_to_set_prob(market: &Market, idx: usize, prob: f64) -> f64 {
-    let current_prob = price(market, idx);
-    market.b * ((prob / current_prob).ln() - ((1.0 - prob) / (1.0 - current_prob)).ln())
+    pub fn shares_to_set_price(&self, idx: usize, new_price: f64) -> f64 {
+        let current_price = self.price(idx);
+        self.b * ((new_price / current_price).ln() - ((1.0 - new_price) / (1.0 - current_price)).ln())
+    }
 }
 
 #[cfg(test)]
@@ -72,7 +70,7 @@ mod tests {
 
         let market = Market { b, outstanding_shares };
 
-        let result = cost_fn(&market);
+        let result = market.cost_fn();
         let expected = 74.439666_f64;
 
         assert_within_epsilon(expected, result);
@@ -91,9 +89,8 @@ mod tests {
         let outstanding_shares = vec!(0_f64, 0_f64);
 
         let market = Market { b, outstanding_shares };
-        let trade = Trade { outcome_id: 0, shares: 10.0 };
 
-        let result = cost_to_trade(&market, &trade);
+        let result = market.cost_to_trade(0, 10.0);
         let expected = 5.124947;
 
         assert_within_epsilon(expected, result);
@@ -106,8 +103,8 @@ mod tests {
 
         let market = Market { b, outstanding_shares };
 
-        let price_0 = price(&market, 0);
-        let price_1 = price(&market, 1);
+        let price_0 = market.price(0);
+        let price_1 = market.price(1);
 
         assert_within_epsilon(price_0, price_1);
         assert_within_epsilon(price_0, 0.5_f64);
@@ -120,14 +117,14 @@ mod tests {
 
         let market = Market { b, outstanding_shares };
 
-        let price_0 = price(&market, 0);
-        let price_1 = price(&market, 1);
+        let price_0 = market.price(0);
+        let price_1 = market.price(1);
 
         assert_within_epsilon(price_0 + price_1, 1.0);
     }
 
     #[test]
-    fn shares_to_set_prob_works() {
+    fn shares_to_set_price_works() {
         let b = 100_f64;
         let shares_0 = 40_f64;
         let shares_1 = 12_f64;
@@ -138,13 +135,13 @@ mod tests {
         let target = 0.6_f64;
         let outcome_id = 1;
 
-        let shares_to_buy = shares_to_set_prob(&market, outcome_id, target);
+        let shares_to_buy = market.shares_to_set_price(outcome_id, target);
 
 
         let outstanding_shares = vec!(shares_0, shares_1 + shares_to_buy);
         let market = Market { b, outstanding_shares };
 
-        let result = price(&market, outcome_id);
+        let result = market.price(outcome_id);
 
         assert_within_epsilon(target, result);
     }
