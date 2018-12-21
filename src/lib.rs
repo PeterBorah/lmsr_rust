84456c213a20765538a175a400dc2947cd7b545b
Peter Borah <peterborah@gmail.com>
2018-12-20 17:47:40 -0800

    Add shares_to_set_prob and simplify tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub fn cost_fn(market: &Market) -> f64 {
 }
 
 fn sum_of_exp(market: &Market) -> f64 {
-    market.outstanding_shares.iter().fold(0_f64, |acc, x| acc + E.pow(x / market.b))
+    market.outstanding_shares.iter().fold(0_f64, |acc, q| acc + E.pow(q / market.b))
 }
 
 pub fn cost_to_trade(market: &Market, trade: &Trade) -> f64 {
@@ -33,6 +33,11 @@ pub fn price(market: &Market, idx: usize) -> f64 {
     E.pow(market.outstanding_shares[idx] / market.b) / sum_of_exp(market)
 }
 
+pub fn shares_to_set_prob(market: &Market, idx: usize, prob: f64) -> f64 {
+    let current_prob = price(market, idx);
+    market.b * ((prob / current_prob).ln() - ((1.0 - prob) / (1.0 - current_prob)).ln())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,11 +46,15 @@ mod tests {
 
     use rug::ops::Pow;
 
+    fn assert_within_epsilon(x: f64, y: f64) {
+        assert!((x - y).abs() < 0.0001, "{} and {} aren't within epsilon", x, y);
+    }
+
     #[test]
     fn e_works() {
         let result = f64::consts::E;
         let expected = 2.71828_f64;
-        assert!((expected - result).abs() < 0.0001);
+        assert_within_epsilon(expected, result);
     }
 
     #[test]
@@ -53,7 +62,7 @@ mod tests {
         let e = f64::consts::E;
         let result = e.pow(2);
         let expected = 7.3890560_f64;
-        assert!((expected - result).abs() < 0.0001);
+        assert_within_epsilon(expected, result);
     }
 
     #[test]
@@ -66,7 +75,7 @@ mod tests {
         let result = cost_fn(&market);
         let expected = 74.439666_f64;
 
-        assert!((expected - result).abs() < 0.0001, "Got {}, expected {}", result, expected);
+        assert_within_epsilon(expected, result);
     }
 
     #[test]
@@ -85,9 +94,9 @@ mod tests {
         let trade = Trade { outcome_id: 0, shares: 10.0 };
 
         let result = cost_to_trade(&market, &trade);
-        let expected = 5.12;
+        let expected = 5.124947;
 
-        assert!((expected - result).abs() < 0.01, "Got {}, expected {}", result, expected);
+        assert_within_epsilon(expected, result);
     }
 
     #[test]
@@ -100,8 +109,8 @@ mod tests {
         let price_0 = price(&market, 0);
         let price_1 = price(&market, 1);
 
-        assert!((price_0 - 0.5).abs() < 0.0001);
-        assert!((price_1 - 0.5).abs() < 0.0001);
+        assert_within_epsilon(price_0, price_1);
+        assert_within_epsilon(price_0, 0.5_f64);
     }
 
     #[test]
@@ -114,6 +123,29 @@ mod tests {
         let price_0 = price(&market, 0);
         let price_1 = price(&market, 1);
 
-        assert!(((price_0 + price_1) - 1.0).abs() < 0.0001, "Inconsistent prices: {}, {}", price_0, price_1);
+        assert_within_epsilon(price_0 + price_1, 1.0);
+    }
+
+    #[test]
+    fn shares_to_set_prob_works() {
+        let b = 100_f64;
+        let shares_0 = 40_f64;
+        let shares_1 = 12_f64;
+
+        let outstanding_shares = vec!(shares_0, shares_1);
+        let market = Market { b, outstanding_shares };
+
+        let target = 0.6_f64;
+        let outcome_id = 1;
+
+        let shares_to_buy = shares_to_set_prob(&market, outcome_id, target);
+
+
+        let outstanding_shares = vec!(shares_0, shares_1 + shares_to_buy);
+        let market = Market { b, outstanding_shares };
+
+        let result = price(&market, outcome_id);
+
+        assert_within_epsilon(target, result);
     }
 }
