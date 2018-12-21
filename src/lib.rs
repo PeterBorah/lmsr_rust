fffaba64de47f6eebe4b65c0a3d1fd25aee1bf2c
Peter Borah <peterborah@gmail.com>
2018-12-20 23:20:07 -0800

    Add MarketMaker::new()

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,23 @@
 extern crate rug;
+
 use std::f64::consts::E;
+use std::collections::HashMap;
 
 use rug::ops::Pow;
 
 #[derive(Clone)]
-pub struct Market {
+pub struct MarketMaker {
     pub b: f64,
     pub outstanding_shares: Vec<f64>,
 }
 
-impl Market {
+impl MarketMaker {
+    pub fn new(b: f64, num_outcomes: usize) -> MarketMaker {
+        let outstanding_shares = vec![0_f64; num_outcomes];
+
+        MarketMaker { b, outstanding_shares }
+    }
+
     pub fn cost_fn(&self) -> f64 {
         self.b * self.sum_of_exp().ln()
     }
@@ -20,10 +28,10 @@ impl Market {
     }
 
     pub fn cost_to_trade(&self, outcome_id: usize, shares: f64) -> f64 {
-        let mut new_market = self.clone();
-        new_market.outstanding_shares[outcome_id] += shares;
+        let mut new_market_maker = self.clone();
+        new_market_maker.outstanding_shares[outcome_id] += shares;
 
-        new_market.cost_fn() - self.cost_fn()
+        new_market_maker.cost_fn() - self.cost_fn()
     }
 
     pub fn price(&self, outcome_id: usize) -> f64 {
@@ -35,12 +43,22 @@ impl Market {
         self.b * ((new_price / current_price).ln() - ((1.0 - new_price) / (1.0 - current_price)).ln())
     }
 
-    pub fn trade(self, outcome_id: usize, shares: f64) -> Market {
-        let mut new_market = self.clone();
-        new_market.outstanding_shares[outcome_id] += shares;
+    pub fn trade(self, outcome_id: usize, shares: f64) -> MarketMaker {
+        let mut new_market_maker = self.clone();
+        new_market_maker.outstanding_shares[outcome_id] += shares;
 
-        new_market
+        new_market_maker
     }
+}
+
+pub struct Portfolio {
+    pub outcome_tokens: Vec<f64>,
+    pub collateral: f64,
+}
+
+pub struct Market {
+    market_maker: MarketMaker,
+    portfolios: HashMap<String, Portfolio>,
 }
 
 #[cfg(test)]
@@ -75,9 +93,9 @@ mod tests {
         let b = 100_f64;
         let outstanding_shares = vec!(10_f64, 0_f64);
 
-        let market = Market { b, outstanding_shares };
+        let market_maker = MarketMaker { b, outstanding_shares };
 
-        let result = market.cost_fn();
+        let result = market_maker.cost_fn();
         let expected = 74.439666_f64;
 
         assert_within_epsilon(expected, result);
@@ -92,12 +110,9 @@ mod tests {
 
     #[test]
     fn cost_to_trade_works() {
-        let b = 100_f64;
-        let outstanding_shares = vec!(0_f64, 0_f64);
+        let market_maker = MarketMaker::new(100.0, 2);
 
-        let market = Market { b, outstanding_shares };
-
-        let result = market.cost_to_trade(0, 10.0);
+        let result = market_maker.cost_to_trade(0, 10.0);
         let expected = 5.124947;
 
         assert_within_epsilon(expected, result);
@@ -105,13 +120,10 @@ mod tests {
 
     #[test]
     fn price_works() {
-        let b = 100_f64;
-        let outstanding_shares = vec!(0_f64, 0_f64);
+        let market_maker = MarketMaker::new(100.0, 2);
 
-        let market = Market { b, outstanding_shares };
-
-        let price_0 = market.price(0);
-        let price_1 = market.price(1);
+        let price_0 = market_maker.price(0);
+        let price_1 = market_maker.price(1);
 
         assert_within_epsilon(price_0, price_1);
         assert_within_epsilon(price_0, 0.5_f64);
@@ -122,10 +134,10 @@ mod tests {
         let b = 100_f64;
         let outstanding_shares = vec!(44_f64, 17_f64);
 
-        let market = Market { b, outstanding_shares };
+        let market_maker = MarketMaker { b, outstanding_shares };
 
-        let price_0 = market.price(0);
-        let price_1 = market.price(1);
+        let price_0 = market_maker.price(0);
+        let price_1 = market_maker.price(1);
 
         assert_within_epsilon(price_0 + price_1, 1.0);
     }
@@ -137,32 +149,30 @@ mod tests {
         let shares_1 = 12_f64;
 
         let outstanding_shares = vec!(shares_0, shares_1);
-        let market = Market { b, outstanding_shares };
+        let market_maker = MarketMaker { b, outstanding_shares };
 
         let target = 0.6_f64;
         let outcome_id = 1;
 
-        let shares_to_buy = market.shares_to_set_price(outcome_id, target);
+        let shares_to_buy = market_maker.shares_to_set_price(outcome_id, target);
 
 
         let outstanding_shares = vec!(shares_0, shares_1 + shares_to_buy);
-        let market = Market { b, outstanding_shares };
+        let market_maker = MarketMaker { b, outstanding_shares };
 
-        let result = market.price(outcome_id);
+        let result = market_maker.price(outcome_id);
 
         assert_within_epsilon(target, result);
     }
 
     #[test]
-    fn trade_works() {
-        let b = 100_f64;
-        let outstanding_shares = vec!(0_f64, 0_f64);
+    fn trade_works_on_mm() {
+        let market_maker = MarketMaker::new(100.0, 2);
         let trade_size = 10.0;
 
-        let market = Market { b, outstanding_shares };
-        let market = market.trade(0, trade_size);
+        let market_maker = market_maker.trade(0, trade_size);
 
-        let result = market.outstanding_shares[0];
+        let result = market_maker.outstanding_shares[0];
 
         assert_within_epsilon(result, trade_size);
     }
